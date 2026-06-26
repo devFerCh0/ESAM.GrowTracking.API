250ac9a0e067b60d55e001a0b8aa6b1886f511ba
Luis Fernando Flores Padilla <dev.fercho@gmail.com>
2026-06-26 00:42:53 -0400

    Break Update 26/06/2026 00:42 - devFerCho

--- ESAM.GrowTracking.Application/Features/Auth/Logout/LogoutCommand.cs
+++ ESAM.GrowTracking.Application/Features/Auth/Logout/LogoutCommand.cs
@@ -3,18 +3,28 @@ using MediatR;
 
 namespace ESAM.GrowTracking.Application.Features.Auth.Logout
 {
-    public sealed record LogoutCommand : IRequest<Result> { } // 1, 2, 3
+    public record LogoutCommand : IRequest<Result> { }
 
     //public record LogoutCommand : IRequest<Result>
     //{
     //    public string? RefreshTokenRaw { get; init; }
 
-    //    public string? DeviceIdentifier { get; init; }
-
-    //    public LogoutCommand(string? refreshTokenRaw, string? deviceIdentifier)
+    //    public LogoutCommand(string? refreshTokenRaw)
     //    {
     //        RefreshTokenRaw = refreshTokenRaw;
-    //        DeviceIdentifier = deviceIdentifier;
     //    }
     //}
+
+    ////public record LogoutCommand : IRequest<Result>
+    ////{
+    ////    public string? RefreshTokenRaw { get; init; }
+
+    ////    public string? DeviceIdentifier { get; init; }
+
+    ////    public LogoutCommand(string? refreshTokenRaw, string? deviceIdentifier)
+    ////    {
+    ////        RefreshTokenRaw = refreshTokenRaw;
+    ////        DeviceIdentifier = deviceIdentifier;
+    ////    }
+    ////}
 }
